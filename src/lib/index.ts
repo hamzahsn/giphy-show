271a406fc0f9e63910d97a3fe8ceb68e1b72312b
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-17 21:50:08 +0100

    export Lib for global usage

---
+++ src/lib/index.ts
@@ -0,0 +1,4 @@
+export { LabelInput } from './LabelInput/LabelInput'
+export { Button } from './Button/Button'
+export { Spinner } from './Spinner/Spinner'
+export { Wrapper } from './Wrapper/Wrapper'
