4b9a608d36e5b5e6b6841e8a37a04053d40a51c4
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-17 18:23:39 +0100

    Enable mock service worker on test environment

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
 
+if (process.env.NODE_ENV === 'test') {
+  const { worker } = require('./mock/browser')
+  worker.start()
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
