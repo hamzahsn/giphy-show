55a1bfe29d280fe432d7aa50b480e878fb01f241
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-17 22:43:05 +0100

    Add title for trendings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Trendings, SearchGiphy } from './components/index'
+import { Wrapper } from '@styles/Wrapper/Wrapper'
 import './App.scss'
 
 export function App() {
   return (
     <>
       <SearchGiphy />
+      <Wrapper title="Trending" />
       <Trendings />
     </>
   )
