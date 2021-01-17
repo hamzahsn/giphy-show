16bef418259b91220dd0835d2d8eb15360304b6a
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-17 18:21:51 +0100

    Adding type definitions for svg and scss and css

---
+++ definitions.d.ts
@@ -0,0 +1,13 @@
+declare module '*.css' {
+  const content: { [className: string]: string }
+  export default content
+}
+declare module '*.scss' {
+  const content: { [className: string]: string }
+  export default content
+}
+
+declare module '*.svg' {
+  const content: any
+  export default content
+}
