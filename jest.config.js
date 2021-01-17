a006b2f35d0a888bbf6b00725509190419d72e86
hamzahsn <amdouni.hamza@hotmail.fr>
2021-01-17 18:22:23 +0100

    chore: fix lint

--- jest.config.js
+++ jest.config.js
@@ -1,38 +1,38 @@
-process.env.NODE_ENV = "test";
+process.env.NODE_ENV = 'test'
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  rootDir: "src",
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  rootDir: 'src',
   testMatch: [
-    "<rootDir>/__tests__/**/*.{test,spec}.(ts|tsx|js|jsx)",
-    "<rootDir>/**/*.{test,spec}.(ts|tsx|js|jsx)",
-    "<rootDir>/**/**/*.{test,spec}.(ts|tsx|js|jsx)",
+    '<rootDir>/__tests__/**/*.{test,spec}.(ts|tsx|js|jsx)',
+    '<rootDir>/**/*.{test,spec}.(ts|tsx|js|jsx)',
+    '<rootDir>/**/**/*.{test,spec}.(ts|tsx|js|jsx)'
   ],
-  coverageDirectory: "<rootDir>/__tests__/__coverage__/",
-  moduleDirectories: ["node_modules", "src"],
+  coverageDirectory: '<rootDir>/__tests__/__coverage__/',
+  moduleDirectories: ['node_modules', 'src'],
   collectCoverageFrom: [
-    "<rootDir>/**/*.tsx",
-    "<rootDir>/**/**/*.tsx",
-    "!<rootDir>/index.tsx",
-    "!<rootDir>/**/*.d.ts",
-    "!<rootDir>/test/mock/*.(ts|tsx)",
+    '<rootDir>/**/*.tsx',
+    '<rootDir>/**/**/*.tsx',
+    '!<rootDir>/index.tsx',
+    '!<rootDir>/**/*.d.ts',
+    '!<rootDir>/test/mock/*.(ts|tsx)'
   ],
-  roots: ["<rootDir>"],
+  roots: ['<rootDir>'],
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFilesAfterEnv: [
-    "<rootDir>/__tests__/config/importJestDOM.ts",
-    "<rootDir>/__tests__/config/setupTest.ts",
-    "<rootDir>/mock/setupServer.ts",
+    '<rootDir>/__tests__/config/importJestDOM.ts',
+    '<rootDir>/__tests__/config/setupTest.ts',
+    '<rootDir>/mock/setupServer.ts'
   ],
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__tests__/__mock__/fileMock.ts",
-    "\\.(css|scss)$": "<rootDir>/__tests__/__mock__/styleMock.ts",
-    "^@components(.*)$": "<rootDir>/components$1",
-    "^@styles(.*)$": "<rootDir>/lib$1",
-    "^@services(.*)$": "<rootDir>/services$1",
-  },
-};
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/__tests__/__mock__/fileMock.ts',
+    '\\.(css|scss)$': '<rootDir>/__tests__/__mock__/styleMock.ts',
+    '^@components(.*)$': '<rootDir>/components$1',
+    '^@styles(.*)$': '<rootDir>/lib$1',
+    '^@services(.*)$': '<rootDir>/services$1'
+  }
+}
